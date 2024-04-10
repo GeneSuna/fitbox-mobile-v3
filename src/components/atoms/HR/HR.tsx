5a5f40c13efa5ca815fd3bb13be4ee07f9f2949d
Lee Alden <bleealden@gmail.com>
2024-04-10 10:14:14 +0800

    applied code formatter and fix type errors

--- src/components/atoms/HR/HR.tsx
+++ src/components/atoms/HR/HR.tsx
@@ -1,24 +1,30 @@
-import { config } from "@/theme/_config";
-import { StyleSheet, View } from "react-native";
+import { config } from '@/theme/_config';
+import { StyleSheet, View } from 'react-native';
 
 type HRProps = {
-    color?: string;
-    thickness?: number;
-    margin?: number;
+	color?: string;
+	thickness?: number;
+	margin?: boolean;
 };
 
 const HR = (props: HRProps) => {
-    const { color, thickness, margin } = props;
+	const { color, thickness, margin } = props;
 
-    return (
-        <View
-            style={{
-                borderColor: color || config.borders.colors.gray,
-                borderWidth: thickness || StyleSheet.hairlineWidth,
-                marginVertical: margin ? config.metrics.rg : config.metrics.xs,
-            }}
-        />
-    );
+	return (
+		<View
+			style={{
+				borderColor: color || config.borders.colors.gray,
+				borderWidth: thickness || StyleSheet.hairlineWidth,
+				marginVertical: margin ? config.metrics.rg : config.metrics.xs,
+			}}
+		/>
+	);
+};
+
+HR.defaultProps = {
+	color: config.borders.colors.gray,
+	thickness: StyleSheet.hairlineWidth,
+	margin: true,
 };
 
 export default HR;
