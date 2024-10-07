ddb55bd3567d34c5facaa60fe042fb5a60cdefb6
Lee Alden Baguio <48677428+iamleealden@users.noreply.github.com>
2024-10-07 20:11:24 +0800

    fix: 5186 Health Form not showing post signup (#148)

--- src/screens/Startup/Startup.tsx
+++ src/screens/Startup/Startup.tsx
@@ -59,7 +59,7 @@ const Startup = ({ navigation }: ApplicationScreenProps) => {
 					}
 
 					if (
-						user.user_data.last_login &&
+						user.user_data.last_login === null &&
 						!user.user_data.is_health_captured
 					) {
 						return navigation.reset({
